cc7d42511f1d902675a328feb4282cd9ec49672c
Paweł Kuśmirowski <56277371+pkkw777@users.noreply.github.com>
2022-02-22 22:19:21 +0100

    Added new controllers

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,109 @@
+﻿using InernetVotingApplication.Models;
+using InernetVotingApplication.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace InernetVotingApplication.Controllers
+{
+    public class AdminController : Controller
+    {
+        private readonly ElectionService _electionService;
+        private readonly AdminService _adminService;
+
+        public AdminController(ElectionService electionService, AdminService adminService)
+        {
+            _electionService = electionService;
+            _adminService = adminService;
+        }
+
+        public IActionResult AddCandidate()
+        {
+            if (string.IsNullOrEmpty(HttpContext.Session.GetString("Admin")))
+            {
+                return RedirectToAction("Login");
+            }
+
+            List<DataWyborow> electionIdList = new();
+            electionIdList = _electionService.ShowElectionByName();
+
+            ViewBag.IdWybory = (List<SelectListItem>)electionIdList.ConvertAll(a =>
+            {
+                return new SelectListItem()
+                {
+                    Text = a.Opis,
+                    Value = a.Id.ToString(),
+                    Selected = false
+                };
+            });
+
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddCandidateAsync(Kandydat kandydat)
+        {
+            if (ModelState.IsValid)
+            {
+                if (await _adminService.AddCandidate(kandydat))
+                {
+                    ViewBag.addCandidateSuccessful = "Kandydat " + kandydat.Imie + " " + kandydat.Nazwisko + " został dodany do głosowania wyborczego!";
+                }
+                else
+                {
+                    ViewBag.Error = false;
+                }
+            }
+
+            List<DataWyborow> electionIdList = new();
+            electionIdList = _electionService.ShowElectionByName();
+
+            ViewBag.IdWybory = (List<SelectListItem>)electionIdList.ConvertAll(a =>
+            {
+                return new SelectListItem()
+                {
+                    Text = a.Opis,
+                    Value = a.Id.ToString(),
+                    Selected = false
+                };
+            });
+
+            return View();
+        }
+
+        public IActionResult CreateElection()
+        {
+            if (string.IsNullOrEmpty(HttpContext.Session.GetString("Admin")))
+            {
+                return RedirectToAction("Login");
+            }
+
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateElectionAsync(DataWyborow dataWyborow)
+        {
+            if (string.IsNullOrEmpty(HttpContext.Session.GetString("Admin")))
+            {
+                return RedirectToAction("Login");
+            }
+
+            if (ModelState.IsValid)
+            {
+                if (await _adminService.AddElectionAsync(dataWyborow))
+                {
+                    ViewBag.addElectionSuccessful = "Wybory zostały dodane!";
+                }
+                else
+                {
+                    ViewBag.Error = false;
+                }
+            }
+
+            return View();
+        }
+    }
+}
---
+++ Controllers/ElectionController.cs
@@ -0,0 +1,186 @@
+﻿using InernetVotingApplication.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+using Array = InernetVotingApplication.ExtensionMethods.ArrayExtensions;
+
+namespace InernetVotingApplication.Controllers
+{
+    public class ElectionController : Controller
+    {
+
+        private readonly ElectionService _electionService;
+        private static readonly object obj = new();
+        public ElectionController(ElectionService electionService)
+        {
+            _electionService = electionService;
+        }
+
+        public IActionResult Dashboard()
+        {
+            if (string.IsNullOrEmpty(HttpContext.Session.GetString("email")))
+            {
+                return RedirectToAction("Login");
+            }
+
+            var vm = _electionService.GetAllElections();
+            return View(vm);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> VotingAsync(int id)
+        {
+            if (string.IsNullOrEmpty(HttpContext.Session.GetString("email")))
+            {
+                return RedirectToAction("Login");
+            }
+
+            if (!string.IsNullOrEmpty(HttpContext.Session.GetString("Admin")))
+            {
+                return RedirectToAction("Panel");
+            }
+
+            if (_electionService.CheckElectionBlockchain(id))
+            {
+                if (_electionService.CheckIfElectionEnded(id))
+                {
+                    return RedirectToAction("ElectionResult", new { @ver = 3, @result = id });
+                }
+
+                if (_electionService.CheckIfElectionStarted(id))
+                {
+                    return RedirectToAction("ElectionResult", new { @ver = 1, @result = id });
+                }
+
+                if (await _electionService.CheckIfVoted(HttpContext.Session.GetString("email"), id))
+                {
+                    return RedirectToAction("ElectionResult", new { @ver = 2, @result = id });
+                }
+
+                @ViewBag.ID = id;
+
+                var vm = _electionService.GetAllCandidates(id);
+                return View(vm);
+            }
+            return RedirectToAction("ElectionError");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> VotingAddAsync(int[] candidate, int[] election)
+        {
+            if (string.IsNullOrEmpty(HttpContext.Session.GetString("email")))
+            {
+                return RedirectToAction("Login");
+            }
+
+            if (Array.IsNullOrEmpty(candidate) || Array.IsNullOrEmpty(election))
+            {
+                return RedirectToAction("Dashboard");
+            }
+
+            int candidateId = candidate[0];
+            int electionId = election[0];
+
+            if (await _electionService.CheckIfVoted(HttpContext.Session.GetString("email"), electionId))
+            {
+                return RedirectToAction("ElectionResult");
+            }
+
+            string ifAdded = "";
+
+            lock (obj)
+            {
+                ifAdded = _electionService.AddVote(HttpContext.Session.GetString("email"), candidateId, electionId);
+            }
+
+            string userHash = ifAdded;
+            if (ifAdded.Length > 3)
+            {
+                ifAdded = "True";
+            }
+            else
+            {
+                ifAdded = "False";
+            }
+
+            if (Convert.ToBoolean(ifAdded))
+            {
+                return RedirectToAction("Voted", new { hash = userHash });
+            }
+            else
+            {
+                return RedirectToAction("ElectionError");
+            }
+        }
+
+        public IActionResult ElectionError()
+        {
+            if (string.IsNullOrEmpty(HttpContext.Session.GetString("email")))
+            {
+                return RedirectToAction("Login");
+            }
+
+            return View();
+        }
+
+        public IActionResult Voted(string hash)
+        {
+            if (string.IsNullOrEmpty(HttpContext.Session.GetString("email")))
+            {
+                return RedirectToAction("Login");
+            }
+
+            @ViewBag.ID = hash;
+            return View();
+        }
+
+        public IActionResult ElectionResult(int ver, int result)
+        {
+            if (string.IsNullOrEmpty(HttpContext.Session.GetString("email")))
+            {
+                return RedirectToAction("Login");
+            }
+
+            if (_electionService.CheckElectionBlockchain(result))
+            {
+                @ViewBag.ID = ver;
+
+                if (ver == 3)
+                {
+                    var vm = _electionService.GetElectionResult(result);
+                    return View(vm);
+                }
+
+                return View();
+            }
+            return RedirectToAction("ElectionError");
+        }
+
+        public IActionResult Search(string Text)
+        {
+            if (string.IsNullOrEmpty(Text))
+            {
+                Text = "1";
+            }
+            var vm = _electionService.SearchVote(Text);
+
+            return View(vm);
+        }
+
+        public IActionResult Panel()
+        {
+            if (string.IsNullOrEmpty(HttpContext.Session.GetString("email")))
+            {
+                return RedirectToAction("Login");
+            }
+
+            if (string.IsNullOrEmpty(HttpContext.Session.GetString("Admin")))
+            {
+                return RedirectToAction("Login");
+            }
+
+            return View();
+        }
+    }
+}
