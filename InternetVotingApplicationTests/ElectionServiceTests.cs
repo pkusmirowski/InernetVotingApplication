0e7555fdaccb4d6650f95c1dc1dae6d5c1c56795
Paweł Kuśmirowski <56277371+pkkw777@users.noreply.github.com>
2022-03-30 21:08:55 +0200

    Added few tests

---
+++ InternetVotingApplicationTests/ElectionServiceTests.cs
@@ -0,0 +1,38 @@
+﻿using InernetVotingApplication.Models;
+using InernetVotingApplication.Services;
+using Microsoft.EntityFrameworkCore;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InternetVotingApplicationTests
+{
+    [TestFixture]
+    public class ElectionServiceTests
+    {
+        private DbContextOptions<InternetVotingContext>? options;
+
+        [SetUp]
+        public void SetUp()
+        {
+            options = new DbContextOptionsBuilder<InternetVotingContext>().UseInMemoryDatabase(databaseName: "temp_DB").Options;
+        }
+
+        [Test]
+        public void GetAllElectionTest()
+        {
+            var context = new InternetVotingContext(options);
+            var repository = new ElectionService(context);
+            var test = repository.GetAllElections();
+            Assert.IsNotNull(test);
+        }
+
+        [Test]
+        public void AddVoteTest()
+        {
+        }
+    }
+}
