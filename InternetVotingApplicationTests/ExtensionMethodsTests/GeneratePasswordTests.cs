397852c6e98d7071379b0c0d1a307bd7ee283ae5
Paweł Kuśmirowski <56277371+pkkw777@users.noreply.github.com>
2022-04-02 14:00:27 +0200

    Added generate password test

---
+++ InternetVotingApplicationTests/ExtensionMethodsTests/GeneratePasswordTests.cs
@@ -0,0 +1,18 @@
+﻿using InernetVotingApplication.ExtensionMethods;
+using NUnit.Framework;
+
+namespace InternetVotingApplicationTests.ExtensionMethodsTests
+{
+    [TestFixture]
+    public class GeneratePasswordTests
+    {
+        [Test]
+        public void GenerateRandomPassword_ResultsNotEmpty()
+        {
+            const int length = 10;
+            var result = GeneratePassword.CreateRandomPassword(length);
+            Assert.NotNull(result);
+            Assert.That(result.Length, Is.EqualTo(length));
+        }
+    }
+}
